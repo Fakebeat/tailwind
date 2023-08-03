195b8f2fc4275f2e1e6816e1fccaa54748b8a124
fakebeat <michaelderiquito1225@gmail.com>
2023-08-03 13:56:58 +0800

    header

--- dist/srcipt.js/script.js
+++ dist/srcipt.js/script.js
@@ -1,6 +1,12 @@
-const toggleMenu = document.querySelector(".toggle__menu");
+// const toggleMenu = document.querySelector(".toggle__menu");
+// const headerNav = document.querySelector(".header__nav");
+// toggleMenu.addEventListener("click", () => {
+//     toggleMenu.classList.toggle("open");
+//     headerNav.classList.toggle("!h[-20rem");
+// })
+
 const headerNav = document.querySelector(".header__nav");
+const toggleMenu = document.querySelector(".toggle__menu");
 toggleMenu.addEventListener("click", () => {
-    toggleMenu.classList.toggle("open");
-    headerNav.classList.toggle("!h[-20rem");
-})
+headerNav.classList.toggle("open");
+});
